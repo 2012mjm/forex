b7565880f559f0884fdb5bc22f3a80209bcfa5a1
javad msd <2007mjm@gmail.com>
2018-10-30 23:42:07 +0330

    check

--- check.js
+++ check.js
@@ -28,63 +28,77 @@ setInterval(() => {
       // let sma1min = ta.SMA.calculate({ period: 14, values: min1.close });
       // let sma5min = ta.SMA.calculate({ period: 14, values: min5.close });
 
-      let macd15sec = ta.MACD.calculate({
-        values: sec15.close,
-        fastPeriod: 5,
-        slowPeriod: 8,
-        signalPeriod: 3,
-        SimpleMAOscillator: false,
-        SimpleMASignal: false
-      });
+      // let macd15sec = ta.MACD.calculate({
+      //   values: sec15.close,
+      //   fastPeriod: 5,
+      //   slowPeriod: 8,
+      //   signalPeriod: 3,
+      //   SimpleMAOscillator: false,
+      //   SimpleMASignal: false
+      // });
 
-      let macd1min = ta.MACD.calculate({
-        values: min1.close,
-        fastPeriod: 5,
-        slowPeriod: 8,
-        signalPeriod: 3,
-        SimpleMAOscillator: false,
-        SimpleMASignal: false
-      });
+      // let macd1min = ta.MACD.calculate({
+      //   values: min1.close,
+      //   fastPeriod: 5,
+      //   slowPeriod: 8,
+      //   signalPeriod: 3,
+      //   SimpleMAOscillator: false,
+      //   SimpleMASignal: false
+      // });
 
-      let macd5min = ta.MACD.calculate({
-        values: min5.close,
-        fastPeriod: 5,
-        slowPeriod: 8,
-        signalPeriod: 3,
-        SimpleMAOscillator: false,
-        SimpleMASignal: false
-      });
+      // let macd5min = ta.MACD.calculate({
+      //   values: min5.close,
+      //   fastPeriod: 5,
+      //   slowPeriod: 8,
+      //   signalPeriod: 3,
+      //   SimpleMAOscillator: false,
+      //   SimpleMASignal: false
+      // });
 
       let rsi15sec = ta.RSI.calculate({ period: 14, values: sec15.close });
       let rsi1min = ta.RSI.calculate({ period: 14, values: min1.close });
       let rsi5min = ta.RSI.calculate({ period: 14, values: min5.close });
 
-      let cci15sec = ta.CCI.calculate({ ...sec15, period: 20 });
-      let cci1min = ta.CCI.calculate({ ...min1, period: 20 });
-      let cci5min = ta.CCI.calculate({ ...min5, period: 20 });
+      // let cci15sec = ta.CCI.calculate({ ...sec15, period: 20 });
+      // let cci1min = ta.CCI.calculate({ ...min1, period: 20 });
+      // let cci5min = ta.CCI.calculate({ ...min5, period: 20 });
 
       // console.log("SMA 15 sec:", sma15sec.slice(-1)[0]);
       // console.log("SMA 1 min:", sma1min.slice(-1)[0]);
       // console.log("SMA 5 min:", sma5min.slice(-1)[0]);
 
-      console.log("MACD 15 sec:", macd15sec.slice(-1)[0]);
-      console.log("MACD 1 min:", macd1min.slice(-1)[0]);
-      console.log("MACD 5 min:", macd5min.slice(-1)[0]);
+      // console.log("MACD 15 sec:", macd15sec.slice(-1)[0]);
+      // console.log("MACD 1 min:", macd1min.slice(-1)[0]);
+      // console.log("MACD 5 min:", macd5min.slice(-1)[0]);
 
       console.log("RSI 15 sec:", rsi15sec.slice(-1)[0]);
       console.log("RSI 1 min:", rsi1min.slice(-1)[0]);
       console.log("RSI 5 min:", rsi5min.slice(-1)[0]);
 
-      console.log("CCI 15 sec:", cci15sec.slice(-1)[0]);
-      console.log("CCI 1 min:", cci1min.slice(-1)[0]);
-      console.log("CCI 5 min:", cci5min.slice(-1)[0]);
+      if (
+        (rsi15sec.slice(-1)[0] < 30 &&
+          rsi1min.slice(-1)[0] < 30 &&
+          rsi5min.slice(-1)[0] < 30) ||
+        (rsi15sec.slice(-1)[0] > 70 &&
+          rsi1min.slice(-1)[0] > 70 &&
+          rsi5min.slice(-1)[0] > 70)
+      ) {
+        let message = `RSI 15 sec: ${rsi15sec.slice(-1)[0]}\nRSI 1 min: ${rsi1min.slice(-1)[0]}\nRSI 5 min: ${rsi5min.slice(-1)[0]}`;
+        chatIds.forEach(chatId => {
+          request(
+            "https://api.telegram.org/bot745759458:AAHtUX6YTX7yxmOIdD_HtSDq7z7IPhL2sh4/sendMessage?chat_id=" +
+              chatId +
+              "&text=" +
+              message
+          );
+        });
+      }
+
+      // console.log("CCI 15 sec:", cci15sec.slice(-1)[0]);
+      // console.log("CCI 1 min:", cci1min.slice(-1)[0]);
+      // console.log("CCI 5 min:", cci5min.slice(-1)[0]);
     }
   );
-
-  let message = "hi";
-  chatIds.forEach(chatId => {
-    //	request('https://api.telegram.org/bot745759458:AAHtUX6YTX7yxmOIdD_HtSDq7z7IPhL2sh4/sendMessage?chat_id='+chatId+'&text='+message);
-  });
 }, 15000);
 
 const fifteenSeconds = results => {
