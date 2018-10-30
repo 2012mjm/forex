24dbbfa5b48376989a48c3a4722b208ceb919c13
javad msd <2007mjm@gmail.com>
2018-10-30 23:26:10 +0330

    check

--- check.js
+++ check.js
@@ -22,9 +22,11 @@ setInterval(() => {
 
       const sec15 = fifteenSeconds(results);
       const min1 = oneMinute(results);
+      const min5 = fiveMinutes(results);
 
       let sma15sec = ta.SMA.calculate({ period: 14, values: sec15.close });
       let sma1min = ta.SMA.calculate({ period: 14, values: min1.close });
+      let sma5min = ta.SMA.calculate({ period: 14, values: min5.close });
 
       let macd15sec = ta.MACD.calculate({
         values: sec15.close,
@@ -44,23 +46,38 @@ setInterval(() => {
         SimpleMASignal: false
       });
 
+      let macd5min = ta.MACD.calculate({
+        values: min5.close,
+        fastPeriod: 5,
+        slowPeriod: 8,
+        signalPeriod: 3,
+        SimpleMAOscillator: false,
+        SimpleMASignal: false
+      });
+
       let rsi15sec = ta.RSI.calculate({ period: 14, values: sec15.close });
       let rsi1min = ta.RSI.calculate({ period: 14, values: min1.close });
+      let rsi5min = ta.RSI.calculate({ period: 14, values: min5.close });
 
       let cci15sec = ta.CCI.calculate({ ...sec15, period: 14 });
       let cci1min = ta.CCI.calculate({ ...min1, period: 14 });
+      let cci5min = ta.CCI.calculate({ ...min5, period: 14 });
 
-      console.log("SMA 15 sec", sma15sec.slice(-1)[0]);
-      console.log("SMA 1 min", sma1min.slice(-1)[0]);
+      console.log("SMA 15 sec:", sma15sec.slice(-1)[0]);
+      console.log("SMA 1 min:", sma1min.slice(-1)[0]);
+      console.log("SMA 5 min:", sma5min.slice(-1)[0]);
 
-      console.log("MACD 15 sec", macd15sec.slice(-1)[0]);
-      console.log("MACD 1 min", macd1min.slice(-1)[0]);
+      console.log("MACD 15 sec:", macd15sec.slice(-1)[0]);
+      console.log("MACD 1 min:", macd1min.slice(-1)[0]);
+      console.log("MACD 5 min:", macd5min.slice(-1)[0]);
 
-      console.log("RSI 15 sec", rsi15sec.slice(-1)[0]);
-      console.log("RSI 1 min", rsi1min.slice(-1)[0]);
+      console.log("RSI 15 sec:", rsi15sec.slice(-1)[0]);
+      console.log("RSI 1 min:", rsi1min.slice(-1)[0]);
+      console.log("RSI 5 min:", rsi5min.slice(-1)[0]);
 
-      console.log("CCI 15 sec", cci15sec.slice(-1)[0]);
-      console.log("CCI 1 min", cci1min.slice(-1)[0]);
+      console.log("CCI 15 sec:", cci15sec.slice(-1)[0]);
+      console.log("CCI 1 min:", cci1min.slice(-1)[0]);
+      console.log("CCI 5 min:", cci5min.slice(-1)[0]);
     }
   );
 
@@ -95,6 +112,35 @@ const oneMinute = results => {
       min.push(result.min);
       max.push(result.max);
     } else if ((i + 1) % 4 == 0) {
+      values.close.push(result.close);
+      min.push(result.min);
+      max.push(result.max);
+      values.high.push(Math.min(...min));
+      values.low.push(Math.max(...max));
+    } else {
+      min.push(result.min);
+      max.push(result.max);
+    }
+
+    i++;
+  });
+  return values;
+};
+
+const fiveMinutes = results => {
+  let i = 0;
+  let min = [];
+  let max = [];
+  let values = { open: [], close: [], high: [], low: [] };
+
+  results.forEach(result => {
+    if (i % 20 == 0) {
+      values.open.push(result.open);
+      min = [];
+      max = [];
+      min.push(result.min);
+      max.push(result.max);
+    } else if ((i + 1) % 20 == 0) {
       values.close.push(result.close);
       min.push(result.min);
       max.push(result.max);
