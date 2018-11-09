e82fdfda83fd53a5b24bb9a9d4b41516a7f10aee
javad msd <2007mjm@gmail.com>
2018-11-09 21:12:26 +0330

    add CandleStick Pattern list

--- checkV2.js
+++ checkV2.js
@@ -141,6 +141,7 @@ const countIf = (array, value) => {
 
 const candleStickPattern = (values, symbol) => {
   let result = [];
+  let bullish = (bearish = 0);
   const val1 = {
     open: values.open[symbol].slice(-1),
     high: values.high[symbol].slice(-1),
@@ -171,69 +172,89 @@ const candleStickPattern = (values, symbol) => {
   }
   if (ta.bearishengulfingpattern(val2)) {
     result.push("Bearish Engulfing");
+    bearish++;
   }
   if (ta.bullishengulfingpattern(val2)) {
     result.push("Bullish Engulfing");
+    bullish++;
   }
   if (ta.darkcloudcover(val2)) {
     result.push("Dark Cloud Cover");
+    bearish++;
   }
   if (ta.downsidetasukigap(val3)) {
     result.push("Downside Tasuki Gap");
+    bearish++;
   }
   if (ta.doji(val1)) {
     result.push("Doji");
   }
   if (ta.dragonflydoji(val1)) {
-    result.push("Dragon Fly Doji");
+    result.push("Dragonfly Doji");
+    bullish++;
   }
   if (ta.gravestonedoji(val1)) {
     result.push("Gravestone Doji");
+    bearish++;
   }
   if (ta.bullishharami(val2)) {
     result.push("Bullish Harami");
+    bullish++;
   }
   if (ta.bearishharami(val2)) {
     result.push("Bearish Harami");
-  }
-  if (ta.bearishharamicross(val2)) {
-    result.push("Bearish Harami Cross");
+    bearish++;
   }
   if (ta.bullishharamicross(val2)) {
     result.push("Bullish Harami Cross");
+    bullish++;
+  }
+  if (ta.bearishharamicross(val2)) {
+    result.push("Bearish Harami Cross");
+    bearish++;
   }
   if (ta.bullishmarubozu(val1)) {
     result.push("Bullish Marubozu");
+    bullish++;
   }
   if (ta.bearishmarubozu(val1)) {
     result.push("Bearish Marubozu");
+    bearish++;
   }
   if (ta.eveningdojistar(val3)) {
     result.push("Evening Doji Star");
+    bearish++;
   }
   if (ta.eveningstar(val3)) {
     result.push("Evening Star");
+    bearish++;
   }
   if (ta.piercingline(val2)) {
     result.push("Piercing Line");
   }
   if (ta.bullishspinningtop(val1)) {
     result.push("Bullish Spinning Top");
+    bullish++;
   }
   if (ta.bearishspinningtop(val1)) {
     result.push("Bearish Spinning Top");
+    bearish++;
   }
   if (ta.morningdojistar(val3)) {
     result.push("Morning Doji Star");
+    bullish++;
   }
   if (ta.morningstar(val3)) {
     result.push("Morning Star");
+    bullish++;
   }
   if (ta.threeblackcrows(val3)) {
     result.push("Three Black Crows");
+    bearish++;
   }
   if (ta.threewhitesoldiers(val3)) {
     result.push("Three White Soldiers");
+    bullish++;
   }
   // if (ta.bullishhammer(val1)) {
   //   result.push("Bullish Hammer");
@@ -249,28 +270,33 @@ const candleStickPattern = (values, symbol) => {
   // }
   if (ta.hammerpattern(val5)) {
     result.push("Hammer");
+    bullish++;
   }
   if (ta.hammerpatternunconfirmed(val5)) {
     result.push("Hammer (Unconfirmed)");
   }
   if (ta.hangingman(val5)) {
     result.push("Hanging Man");
+    bearish++;
   }
   if (ta.hangingmanunconfirmed(val5)) {
     result.push("Hanging Man (Unconfirmed)");
   }
   if (ta.shootingstar(val5)) {
     result.push("Shooting Star");
+    bearish++;
   }
   if (ta.shootingstarunconfirmed(val5)) {
     result.push("Shooting Star (Unconfirmed)");
   }
   if (ta.tweezertop(val5)) {
     result.push("Tweezer Top");
+    bearish++;
   }
   if (ta.tweezerbottom(val5)) {
     result.push("Tweezer Bottom");
+    bullish++;
   }
 
-  return result;
+  return {result, bullish, bearish};
 };
