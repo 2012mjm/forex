98c9bb49395da0e7880f6cdc418d72710190b9f1
javad msd <2007mjm@gmail.com>
2018-11-09 22:40:56 +0330

    check api

--- checkApi.js
+++ checkApi.js
@@ -1,8 +1,10 @@
 const config = require("./config");
 
-const ta = require("technicalindicators");
-const request = require("request");
-const tgBot = require("node-telegram-bot-api");
+const express = require("express");
+const app = express();
+app.listen(3124, () => {
+  console.log("Server running on port 3124");
+});
 
 const mysql = require("mysql");
 const connection = mysql.createConnection({
@@ -14,59 +16,21 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-const tg = new tgBot(config.telegram_bot_api, {
-  polling: true
-});
+app.get(
+  "/candlestick/:date",
+  (req, res) => {
+    connection.query(
+      `SELECT * FROM quotes WHERE symbol = 'EURUSD' AND date <= ${req.params.date} ORDER BY id DESC LIMIT 10`,
+      (error, results, fields) => {
+        results = results.reverse();
+        const sec15 = filter(results);
 
-// config.telegram_chat_ids
-
-setInterval(() => {
-  connection.query(
-    "SELECT * FROM `quotes` ORDER BY id DESC LIMIT 350",
-    (error, results, fields) => {
-      results = results.reverse();
-      const sec15 = filter(results);
-
-      connection.query(
-        "SELECT COALESCE(`id`) id, symbol, COALESCE(`date`) `date`, \
-        MIN(`min`) as `min`, MAX(`max`) as `max`, COALESCE(`open`) as `open`, SUBSTRING_INDEX(GROUP_CONCAT(`close`), ',', -1) as `close` \
-        FROM (SELECT * FROM `quotes` ORDER BY id DESC LIMIT 1500) as t \
-        GROUP BY symbol, DATE_FORMAT(`date`, '%Y-%m-%d %H:%i') \
-        ORDER BY symbol, id DESC",
-        (error, results, fields) => {
-          results = results.reverse();
-          const min1 = filter(results);
-
-          item = "EURUSD";
-          // config.quote_symbols.forEach(item => {
-            // if (countIf(sec15.type[item], "buy") >= 15) {
-            //   console.log(`${item} -> best buy`);
-            // } else if (countIf(sec15.type[item], "sell") >= 15) {
-            //   console.log(`${item} -> best sell`);
-            // }
-
-            console.log(item, candleStickPattern(min1, item));
-            console.log(item, candleStickPattern(sec15, item));
-            console.log("\n");
-
-            // let rsi15sec = ta.RSI.calculate({
-            //   period: 14,
-            //   values: sec15.close[item]
-            // });
-            // let rsi1min = ta.RSI.calculate({
-            //   period: 14,
-            //   values: min1.close[item]
-            // });
-            // console.log(`${item} -> RSI 15 sec:`, rsi15sec.slice(-1)[0]);
-            // console.log(`${item} -> RSI 1 min:`, rsi1min.slice(-1)[0]);
-          // });
-        }
-      );
-
-      console.log("\n\n");
-    }
-  );
-}, 15000);
+        res.json(candleStickPattern(sec15, "EURUSD"));
+      }
+    );
+  },
+  15000
+);
 
 const filter = results => {
   let values = { open: [], close: [], high: [], low: [], type: [] };
@@ -87,58 +51,6 @@ const filter = results => {
     );
   });
   return values;
-};
-
-const bollingerBands = values => {
-  let result = ta.BollingerBands
-    .calculate({
-      period: 20,
-      stdDev: 2,
-      values
-    })
-    .slice(-1)[0];
-
-  const newClose = values.slice(-1)[0];
-
-  if (newClose >= result.upper) {
-    result.status = "upper";
-  } else if (newClose <= result.lower) {
-    result.status = "lower";
-  } else if (newClose > result.middle) {
-    result.status = "top middle";
-  } else if (newClose < result.middle) {
-    result.status = "down middle";
-  } else {
-    result.status = null;
-  }
-  return result;
-};
-
-const parabolicSAR = (low, high) => {
-  let result = ta.PSAR
-    .calculate({
-      step: 0.02,
-      max: 0.2,
-      high,
-      low
-    })
-    .slice(-1)[0];
-
-  const newLow = low.slice(-1)[0];
-  const newHigh = high.slice(-1)[0];
-
-  if (newLow > result || newHigh > result) {
-    result = { value: result, status: "down" };
-  } else if (newLow < result || newHigh < result) {
-    result = { value: result, status: "top" };
-  } else {
-    result = { value: result, status: null };
-  }
-  return result;
-};
-
-const countIf = (array, value) => {
-  return array.filter(x => x == value).length;
 };
 
 const candleStickPattern = (values, symbol) => {
